949886b58c26b0eb48fb35a106c514fcfaf6dd9f
David Alves Tresano <pikachu.dat40@gmail.com>
2022-12-04 00:34:39 -0300

    banco de dados funcional

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const mysql = require('mysql');
 const express = require('express');
 const { WebSocketServer } = require('ws');
 
@@ -12,15 +13,75 @@ const server = express()
 
 const wss = new WebSocketServer({ server: server, autoAcceptConnections: true });
 
-var postItArray;
+var database = mysql.createConnection({
+  host     : 'spigo.net',
+  user     : 'spigo594_grupo02',
+  password : process.env.PASSWORD,
+  database : 'spigo594_grupo02'
+});
+
+database.connect();
+
+database.query("SELECT * FROM postit", function (error, results, fields){ 
+  if (error) throw error;  
+  initialize(results);
+});
+
+var postItArray = [];
+
+function initialize(results){
+  results.forEach(function(result){
+    postItArray[result.id] = {};
+    postItArray[result.id].x = result.x;
+    postItArray[result.id].y = result.y;
+    postItArray[result.id].text = result.text;
+    postItArray[result.id].hue = Math.random() * 360;
+    postItArray[result.id].size = 200;
+    postItArray[result.id].isSelected = false;
+  });
+}
 
 wss.on('connection', (ws) => {
   console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
+  wss.broadcast(JSON.stringify["array", postItArray], server);
+
+  ws.on('close', () => {
+    console.log('Client disconnected')
+    
+    postItArray.forEach(function(postIt, i){
+      let query = `UPDATE postit SET x = ${postIt.x}, y = ${postIt.y}, text = "${postIt.text}" WHERE id = ${i}`;
+      database.query(query, function(error, results, fields){
+        if (error) throw error;
+      });
+    });
+  });
+
   ws.on('message', message => {
     let mensagem = JSON.parse(message);
     if(mensagem[0] == "array"){
+        if(mensagem[1].length > postItArray.length){
+          let postIt = mensagem[1][mensagem[1].length-1];
+          let query = `INSERT INTO postit VALUES (${mensagem[1].length-1}, ${postIt.x}, ${postIt.y}, "${postIt.text}")`;
+          database.query(query, function(error, results, fields){
+            if (error) throw error;
+          });
+        }
+
+        if(mensagem[1].length < postItArray.length){
+          let postIt = postItArray[postItArray.length-1];
+          let query = `DELETE FROM postit WHERE id = ${postItArray.length-1}`;
+          database.query(query, function(error, results, fields){
+            if (error) throw error;
+          });
+        }
+
         postItArray = mensagem[1];
+        postItArray.forEach(function(postIt, i){
+          let query = `UPDATE postit SET x = ${postIt.x}, y = ${postIt.y}, text = "${postIt.text}" WHERE id = ${i}`;
+          database.query(query, function(error, results, fields){
+            if (error) throw error;
+          });
+        });
         wss.broadcast(JSON.stringify(["array", postItArray]), server);
     } else if(mensagem[0] == "connect"){
         wss.broadcast(JSON.stringify(["array", postItArray]), server);
